9efc52b4c8a8b1b8e8a2a413990cd81243a289d2
smartdev <smartdevs17@gmail.com>
2026-01-28 10:53:01 +0100

    refactor: remove SupplyChainPaymentSplitter contract

--- contracts/PaymentSplitter.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-import "@openzeppelin/contracts/finance/PaymentSplitter.sol";
-
-contract SupplyChainPaymentSplitter is PaymentSplitter {
-    constructor(address[] memory payees, uint256[] memory shares) PaymentSplitter(payees, shares) {}
-}
